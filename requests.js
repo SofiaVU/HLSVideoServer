cca87a9241dd87c0b1e0e132477c7b9c7bd593b6
Sergio López <sergiolf96@gmail.com>
2019-04-09 15:11:53 +0200

    No DB

--- requests.js
+++ requests.js
@@ -2,7 +2,7 @@ import {Router} from 'express';
 import {processVideo} from "./ffmpegProcessing";
 import {createServer} from 'http';
 import {ports} from "./server";
-import {User, Video} from "./models";
+
 
 let express = require('express');
 
@@ -13,146 +13,153 @@ let HLSServer = require('hls-server');
 export let router = Router();
 
 router.post('/upload', async(req, res) => {
-    if (!req.files) {
-        return res.status(400).send('No files were uploaded.');
-    }
 
-    let file = req.files.fileToUpload;
+    console.log("Upload")
 
-    let metadata = file.name.match(/(.+)(\.)(\w+)/);
-
-    let name = req.sanitize(req.body.name);
-
-    let newVideo = await Video.create({
-        name: name,
-        userId: req.user.id,
-        status: "processing",
-        port: undefined,
-
-    });
-
-    // res.redirect('http://' + process.env.WEBAPP_SERVER_URL + ':' + process.env.WEBAPP_SERVER_PORT
-    //     + '/video?upload=ok');
-
-    res.status(200).send();
-
-    file.mv('videos/' + file.name, async(err) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-
-        let processOk = await processVideo(metadata[1], metadata[3], newVideo.id);
-
-        if (processOk) {
-            Video.findOne({
-                where: {
-                    id: {
-                        $eq: newVideo.id,
-                    },
-                },
-            }).then(video => {
-                video.status = "ready";
-
-                video.save();
-
-                fs.unlink('videos/' + file.name);
-            });
-        }
-
-    });
+    // if (!req.files) {
+    //     return res.status(400).send('No files were uploaded.');
+    // }
+    //
+    // let file = req.files.fileToUpload;
+    //
+    // let metadata = file.name.match(/(.+)(\.)(\w+)/);
+    //
+    // let name = req.sanitize(req.body.name);
+    //
+    // let newVideo = await Video.create({
+    //     name: name,
+    //     userId: req.user.id,
+    //     status: "processing",
+    //     port: undefined,
+    //
+    // });
+    //
+    // // res.redirect('http://' + process.env.WEBAPP_SERVER_URL + ':' + process.env.WEBAPP_SERVER_PORT
+    // //     + '/video?upload=ok');
+    //
+    // res.status(200).send();
+    //
+    // file.mv('videos/' + file.name, async(err) => {
+    //     if (err) {
+    //         console.log(err);
+    //         return;
+    //     }
+    //
+    //     let processOk = await processVideo(metadata[1], metadata[3], newVideo.id);
+    //
+    //     if (processOk) {
+    //         Video.findOne({
+    //             where: {
+    //                 id: {
+    //                     $eq: newVideo.id,
+    //                 },
+    //             },
+    //         }).then(video => {
+    //             video.status = "ready";
+    //
+    //             video.save();
+    //
+    //             fs.unlink('videos/' + file.name);
+    //         });
+    //     }
+    //
+    // });
 });
 router.get('/play', async(req, res) => {
 
-    let video = await Video.findOne({where: {id: req.query.id}});
+    console.log("play")
 
-    if (video.port !== null) {
-        console.log(video.name + " played on " + video.port);
-        res.send(JSON.stringify({
-            port: video.port,
-        }));
-
-        for (let item in ports) {
-            if (ports[item].port === video.port) {
-                ports[item].listeners++;
-                return;
-            }
-        }
-
-    }
-
-
-    let server = createServer();
-
-    let streaming = 'streams/' + video.id + '/playlist.m3u8';
-
-    new HLSServer(server, {
-        path: '/play',     // Base URI to output HLS streams
-        dir: streaming // Directory that input files are stored
-    });
-
-    for (let item in ports) {
-        if (ports[item].available) {
-            ports[item].available = false;
-            ports[item].server = server;
-            ports[item].listeners = 1;
-
-            server.listen(ports[item].port);
-            server.on('request', (req, res) => {
-                res.setHeader('Access-Control-Allow-Origin', '*');
-            });
-
-            video.port = ports[item].port;
-
-            video.save();
-
-            res.send(JSON.stringify({
-                port: ports[item].port,
-            }));
-
-            return;
-        }
-
-    }
+    // let video = await Video.findOne({where: {id: req.query.id}});
+    //
+    // if (video.port !== null) {
+    //     console.log(video.name + " played on " + video.port);
+    //     res.send(JSON.stringify({
+    //         port: video.port,
+    //     }));
+    //
+    //     for (let item in ports) {
+    //         if (ports[item].port === video.port) {
+    //             ports[item].listeners++;
+    //             return;
+    //         }
+    //     }
+    //
+    // }
+    //
+    //
+    // let server = createServer();
+    //
+    // let streaming = 'streams/' + video.id + '/playlist.m3u8';
+    //
+    // new HLSServer(server, {
+    //     path: '/play',     // Base URI to output HLS streams
+    //     dir: streaming // Directory that input files are stored
+    // });
+    //
+    // for (let item in ports) {
+    //     if (ports[item].available) {
+    //         ports[item].available = false;
+    //         ports[item].server = server;
+    //         ports[item].listeners = 1;
+    //
+    //         server.listen(ports[item].port);
+    //         server.on('request', (req, res) => {
+    //             res.setHeader('Access-Control-Allow-Origin', '*');
+    //         });
+    //
+    //         video.port = ports[item].port;
+    //
+    //         video.save();
+    //
+    //         res.send(JSON.stringify({
+    //             port: ports[item].port,
+    //         }));
+    //
+    //         return;
+    //     }
+    //
+    // }
 
 });
 
 router.get('/destroy', async(req, res) => {
 
-    let port = req.query.port;
+    console.log("Destroy")
 
-    for (let item in ports) {
-
-        if ("" + ports[item].port === port) {
-
-            ports[item].listeners--;
-
-            if (ports[item].listeners === 0) {
-
-                let video = await Video.findOne({where: {id: req.query.id}});
-
-                video.port = null;
-
-                video.save();
-
-                console.log(video.port);
-
-                ports[item].server.close(() => {
-                    console.log("Server down");
-                })
-                ports[item].server = null;
-                ports[item].available = true;
-
-                console.log(ports[item])
-
-
-
-
-            }
-
-            return;
-        }
-    }
+    // let port = req.query.port;
+    //
+    // for (let item in ports) {
+    //
+    //     if ("" + ports[item].port === port) {
+    //
+    //         ports[item].listeners--;
+    //
+    //         if (ports[item].listeners === 0) {
+    //
+    //             let video = await Video.findOne({where: {id: req.query.id}});
+    //
+    //             video.port = null;
+    //
+    //             video.save();
+    //
+    //             console.log(video.port);
+    //
+    //             ports[item].server.close(() => {
+    //                 console.log("Server down");
+    //             })
+    //             ports[item].server = null;
+    //             ports[item].available = true;
+    //
+    //             console.log(ports[item])
+    //
+    //
+    //
+    //
+    //         }
+    //
+    //         return;
+    //     }
+    // }
 
 
 
